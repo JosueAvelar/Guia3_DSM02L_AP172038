549bf2fc292481b515d6506875cc4dbc26a2f981
JosueAvelar <avelarjosue1@gmail.com>
2022-02-25 21:24:04 -0600

    Repositorio para la practica 3 de DSM02L

---
+++ Ejercicio 2_Complementario/app/src/main/java/com/example/complementario2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.complementario2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Buttom btnFrame, btnLinear, btnRelative, btnGrid, btnTable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFrame (View v) {
+        Intent llamar = new Intent(packageContext: this, ejemplo_frame_layout.class);
+        startActivity(llamar);
+    }
+
+    public void onClickLinear (View v) {
+        Intent llamar = new Intent(packageContext; this, ejemplo_linear_layout.class);
+        startActivity(llamar);
+    }
+
+
+    public void onClickRelative (View v) {
+        Intent llamar = new Intent(packageContext; this, ejemplo_relative_layout.class);
+        startActivity(llamar);
+    }
+
+    public void onClickTable (View v) {
+        Intent llamar = new Intent(packageContext; this, ejemplo_table_layout.class);
+        startActivity(llamar);
+    }
+
+    public void onClickGrid (View v) {
+        Intent llamar = new Intent(packageContext; this, ejemplo_grid_layout.class);
+        startActivity(llamar);
+    }
+}
